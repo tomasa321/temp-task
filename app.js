0f5cb74211945eb5d21a90228a208aa3b28a616e
Tomas <tomas@getkevin.io>
2019-10-10 07:54:47 +0300

    getConfigurations function added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const request = require('request-promise');
 
 const inputFilePath = process.argv[2];
+const cashInConfigUrl = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in';
+const cashOutNaturalConfigUrl = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural';
+const cashOutLegalConfigUrl = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical';
 
 function parseFileData(filePath) {
   return new Promise((resolve) => {
@@ -15,7 +19,21 @@ function parseFileData(filePath) {
   });
 }
 
+function getConfigurations() {
+  const promises = [];
+  promises.push(request(cashInConfigUrl));
+  promises.push(request(cashOutNaturalConfigUrl));
+  promises.push(request(cashOutLegalConfigUrl));
+  return Promise.all(promises)
+    .then((configs) => ({
+      cashInConfig: JSON.parse(configs[0]),
+      cashOutNaturalConfig: JSON.parse(configs[1]),
+      cashOutLegalConfig: JSON.parse(configs[2]),
+    }));
+}
+
 parseFileData(inputFilePath).then(console.log).catch((err) => {
   console.log('Error:');
   console.log(err.message);
 });
+getConfigurations().then(console.log);
